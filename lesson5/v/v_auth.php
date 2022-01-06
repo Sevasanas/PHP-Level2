aff3414587bb96173f9dd58fbb79ac416a937e54
Sevasanas <sevanas86@gmail.com>
2022-01-06 11:21:18 +0300

    created authorization

---
+++ lesson5/v/v_auth.php
@@ -0,0 +1,19 @@
+<h3>Login</h3>
+<form  method="post">
+  <label for="login">Login</label>
+  <input type="text" id="login" name="login">
+  <label for="password">Password</label>
+  <input type="password" id="password" name="password">
+  <br>
+  <br>
+  <input type="submit" value="Log in">
+</form>
+<?php if ($message == 'incorrect_login') : ?>
+  <div>Incorrect login and/or password!</div>
+<?php elseif ($message): ?>
+  <div><?= $message ?>, please login again!</div>
+<?php else: ?>
+  <div>Input your login and password</div>
+<?php endif; ?>
+<br>
+<a href="/v/v_registration">Registration</a>
